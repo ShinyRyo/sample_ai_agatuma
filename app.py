47789098857c8759063a02ad4fd152f979875ff6
ShinyRyo <58499405+ShinyRyo@users.noreply.github.com>
2022-04-18 11:12:32 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ import os.path
 import numpy as np
 from openvino.inference_engine import IECore
 
+def read_img(img_file):
+    frame = Image.open(img_file)#cv2.imread('image.jpg') 
+    img = frame.convert("RGB")
+
+    img_size=300
+    # 入力データフォーマットへ変換 
+    img = img.resize((img_size, img_size))
+
+    img = np.array(img, dtype=np.uint8)
+    img = img.transpose((2, 0, 1))
+    img = np.expand_dims(img, axis=0) # 次元合せ 
+    return img
+
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
 st.sidebar.title("画像認識アプリ")
@@ -27,89 +40,89 @@ ie = IECore()
 model_face = 'intel/face-detection-retail-0004/FP32/face-detection-retail-0004.xml'
 weights_face = 'intel/face-detection-retail-0004/FP32/face-detection-retail-0004.bin'
 
-# #モデルの準備（感情分類） 
-# model_emotion = 'intel/emotions-recognition-retail-0003/FP32/emotions-recognition-retail-0003.xml'
-# weights_emotion = 'intel/emotions-recognition-retail-0003/FP32/emotions-recognition-retail-0003.bin'
+#モデルの準備（感情分類） 
+model_emotion = 'intel/emotions-recognition-retail-0003/FP32/emotions-recognition-retail-0003.xml'
+weights_emotion = 'intel/emotions-recognition-retail-0003/FP32/emotions-recognition-retail-0003.bin'
 
-# # モデルの読み込み（顔検出） 
-# net_face = ie.read_network(model=model_face, weights=weights_face)
-# exec_net_face = ie.load_network(network=net_face, device_name='CPU')
+# モデルの読み込み（顔検出） 
+net_face = ie.read_network(model=model_face, weights=weights_face)
+exec_net_face = ie.load_network(network=net_face, device_name='CPU')
 
-# # モデルの読み込み（感情分類） 
-# net_emotion = ie.read_network(model=model_emotion, weights=weights_emotion)
-# exec_net_emotion = ie.load_network(network=net_emotion, device_name='CPU')
+# モデルの読み込み（感情分類） 
+net_emotion = ie.read_network(model=model_emotion, weights=weights_emotion)
+exec_net_emotion = ie.load_network(network=net_emotion, device_name='CPU')
 
-# # 入出力データのキー取得 
-# input_blob_face = next(iter(net_face.input_info))
-# out_blob_face = next(iter(net_face.outputs))
+# 入出力データのキー取得 
+input_blob_face = next(iter(net_face.input_info))
+out_blob_face = next(iter(net_face.outputs))
 
-# input_blob_emotion = next(iter(net_emotion.input_info))
-# out_blob_emotion = next(iter(net_emotion.outputs))
+input_blob_emotion = next(iter(net_emotion.input_info))
+out_blob_emotion = next(iter(net_emotion.outputs))
 
-# frame = Image.open(img_file)
-# frame = np.array(frame, dtype=np.uint8)
-# img = read_img(img_file)
+frame = Image.open(img_file)
+frame = np.array(frame, dtype=np.uint8)
+img = read_img(img_file)
 
-# # 推論実行 
-# out = exec_net_face.infer(inputs={input_blob_face: img})
+# 推論実行 
+out = exec_net_face.infer(inputs={input_blob_face: img})
 
-# # 出力から必要なデータのみ取り出し 
-# out = out[out_blob_face]
-# out = np.squeeze(out) #サイズ1の次元を全て削除 
+# 出力から必要なデータのみ取り出し 
+out = out[out_blob_face]
+out = np.squeeze(out) #サイズ1の次元を全て削除 
 
-# # 検出されたすべての顔領域に対して１つずつ処理 
-# for detection in out:
-#     # conf値の取得 
-#     confidence = float(detection[2])
+# 検出されたすべての顔領域に対して１つずつ処理 
+for detection in out:
+    # conf値の取得 
+    confidence = float(detection[2])
 
-#     # バウンディングボックス座標を入力画像のスケールに変換 
-#     xmin = int(detection[3] * frame.shape[1])
-#     ymin = int(detection[4] * frame.shape[0])
-#     xmax = int(detection[5] * frame.shape[1])
-#     ymax = int(detection[6] * frame.shape[0])
+    # バウンディングボックス座標を入力画像のスケールに変換 
+    xmin = int(detection[3] * frame.shape[1])
+    ymin = int(detection[4] * frame.shape[0])
+    xmax = int(detection[5] * frame.shape[1])
+    ymax = int(detection[6] * frame.shape[0])
 
-#     # conf値が0.5より大きい場合のみバウンディングボックス表示 
-#     if confidence > 0.5:
-#            # 顔検出領域はカメラ範囲内に補正する。特にminは補正しないとエラーになる 
-#             if xmin < 0:
-#                 xmin = 0
-#             if ymin < 0:
-#                 ymin = 0
-#             if xmax > frame.shape[1]:
-#                 xmax = frame.shape[1]
-#             if ymax > frame.shape[0]:
-#                 ymax = frame.shape[0]
+    # conf値が0.5より大きい場合のみバウンディングボックス表示 
+    if confidence > 0.5:
+           # 顔検出領域はカメラ範囲内に補正する。特にminは補正しないとエラーになる 
+            if xmin < 0:
+                xmin = 0
+            if ymin < 0:
+                ymin = 0
+            if xmax > frame.shape[1]:
+                xmax = frame.shape[1]
+            if ymax > frame.shape[0]:
+                ymax = frame.shape[0]
  
-#             # 顔領域のみ切り出し 
-#             frame_face = frame[ ymin:ymax, xmin:xmax ]
+            # 顔領域のみ切り出し 
+            frame_face = frame[ ymin:ymax, xmin:xmax ]
  
-#             # 入力データフォーマットへ変換 
-#             img = np.resize(frame,(64,64,3))
-#             img = img.transpose((2, 0, 1))    # HWC > CHW 
-#             img = np.expand_dims(img, axis=0) # 次元合せ 
+            # 入力データフォーマットへ変換 
+            img = np.resize(frame,(64,64,3))
+            img = img.transpose((2, 0, 1))    # HWC > CHW 
+            img = np.expand_dims(img, axis=0) # 次元合せ 
  
-#             # 推論実行 
-#             out = exec_net_emotion.infer({input_blob_emotion: img})
+            # 推論実行 
+            out = exec_net_emotion.infer({input_blob_emotion: img})
  
-#             # 出力から必要なデータのみ取り出し 
-#             out = out[out_blob_emotion]
-#             out = np.squeeze(out) #不要な次元の削減 
+            # 出力から必要なデータのみ取り出し 
+            out = out[out_blob_emotion]
+            out = np.squeeze(out) #不要な次元の削減 
  
-#             # 出力値が最大のインデックスを得る 
-#             index_max = np.argmax(out)
+            # 出力値が最大のインデックスを得る 
+            index_max = np.argmax(out)
  
-#             # 各感情の文字列をリスト化 
-#             list_emotion = ['neutral', 'happy', 'sad', 'surprise', 'anger']
+            # 各感情の文字列をリスト化 
+            list_emotion = ['neutral', 'happy', 'sad', 'surprise', 'anger']
  
-#             # バウンディングボックス表示 
-#             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(240, 180, 0), thickness=3)
+            # バウンディングボックス表示 
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(240, 180, 0), thickness=3)
 
-#             # 文字列描画 
-#             cv2.putText(frame, list_emotion[index_max], (300, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2) 
+            # 文字列描画 
+            cv2.putText(frame, list_emotion[index_max], (300, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2) 
  
-#             # １つの顔で終了 
-#             break
+            # １つの顔で終了 
+            break
 
-# # 画像表示
-# fig, ax = plt.subplots()
-# ax.imshow(frame)
+# 画像表示
+fig, ax = plt.subplots()
+ax.imshow(frame)
